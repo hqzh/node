f9797eacfb068b52b2a46030b4f4513214f500e2
zhuhuaqiang <616928019@qq.com>
2018-09-07 19:51:22 +0800

    配置npm run start,start可以省略run(之所以取名为server.js不用配置,应该是node做了优化)
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer(function (request, response) {
+app.get('/',(req,res) => {
+	res.send('hello,express');
+});
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end("Hello, Universe! I'm running on Cloud Studio!\n");
-}).listen(80);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+app.listen(3000, () => {
+	console.log('run in localhost:3000');
+});
