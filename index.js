b18db82ca837addaa0559f592743c56bd1015a6e
zhuhuaqiang <616928019@qq.com>
2018-09-12 19:54:42 +0800

    docs: 用回调处理一次性事件,嵌套多多

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
 
-app.get('/',(req,res) => {
-	res.send('hello,express');
-});
-
-app.listen(3000, () => {
-	console.log('run in localhost:3000');
-});
+http.createServer((req,res)=>{
+	if (req.url ==='/') {
+		fs.readFile('./titles.json',(err,data) => {
+			if(err){
+				console.log(err);
+				res.end('server err')
+			}else{
+				const titles = JSON.parse(data,toString());
+				fs.readFile('./template.html',(err,data)=>{
+					if(err){
+						console.log(err);
+						res.end('server err')
+					}else{
+						const templ = data.toString();
+						const html = templ.replace('%',titles.join('<li></li>'));
+						res.writeHead(200,{'Content-Type':'text/html'});
+						res.end(html)
+					}
+				})
+			}
+		})
+	} 
+}).listen(8000,'127.0.0.1')
