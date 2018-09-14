e4effdd7bf541391b55fe13e707c7ec26e18db7e
zhuhuaqiang <616928019@qq.com>
2018-09-14 19:40:10 +0800

    refactor: 通过尽早返回减少嵌套的例子
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req,res)=>{
+http.createServer((req, res) => {
 	getTitles(res);
-}).listen(8000,'127.0.0.1')
+}).listen(8000, '127.0.0.1')
 
 const getTitles = (res) => {
-	fs.readFile('./titles.json',(err,data)=>{
-		if(err){
-			hadError(err,res);
-		}else{
-			getTemplate(JSON.parse(data.toString()),res)
-		}
+	fs.readFile('./titles.json', (err, data) => {
+		if (err) return hadError(err, res);
+		getTemplate(JSON.parse(data.toString()), res)
 	})
 }
 
-const getTemplate = (titles,res) => {
-	fs.readFile('./template.html',(err,data) => {
-		if(err){
-			hadError(err,res);
-		}else{
-			formatHtml(titles,data.toString(),res)
-		}
+const getTemplate = (titles, res) => {
+	fs.readFile('./template.html', (err, data) => {
+		if (err) return hadError(err, res);
+		formatHtml(titles, data.toString(), res)
 	})
 }
 
-const formatHtml = (titles,tmpl,res) => {
-	const html = tmpl.replace('%',titles.join('<li></li>'));
+
+const formatHtml = (titles, tmpl, res) => {
+	const html = tmpl.replace('%', titles.join('<li></li>'));
 	res.end(html);
 }
 
-const hadError = (err,res) => {
+const hadError = (err, res) => {
 	console.log(err);
 	res.end('server err')
 }
