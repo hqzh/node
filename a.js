27950f34934258e94f3f1473ee0e33a3775f854d
zhuhuaqiang <616928019@qq.com>
2018-09-29 17:12:37 +0800

    重新开始搭建node web
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- a.js
+++
@@ -1,3 +0,0 @@
-const a = 8;
-
-exports.a = a;
--- b.js
+++
@@ -1,9 +0,0 @@
-var { a } = require('./a');
-
-console.log(a)
-
-console.log(module.paths)
-
-console.log(require('./c'))
-
-console.log(get_builtin_module())
