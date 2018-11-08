173e99f47ac1ee78ae18c1392cfae03b919f1853
zhuhuaqiang <616928019@qq.com>
2018-11-08 21:32:47 +0800

    :construction: hello kraken
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

---
+++ kraken-js/server.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    kraken = require('kraken-js');
+
+var app = express();
+app.use(kraken());
+app.listen(8000);
---
+++ kraken/README.md
@@ -0,0 +1,4 @@
+kraken
+===========
+
+my kraken
