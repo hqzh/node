666a1ef757521555a20afeaf0cca0ad88d951fdd
zhuhuaqiang <616928019@qq.com>
2018-11-08 19:56:09 +0800

    :construction: explain koa switch of component
    Signed-off-by: zhuhuaqiang <616928019@qq.com>

--- koa-middle/server.js
+++ koa-middle/server.js
@@ -3,12 +3,14 @@ const app = koa();
 
 app.use(function *(next) {
   const start = new Date;
+  // 这个yield是为了执行下一个app.use,然后走下一个app.use,走完再回来走本次yield next下面的部分;
   yield next;
   const ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-app.use(function *() {
+app.use(function *(next) {
+  console.log(111)
   this.body = 'Hello World';
 });
 
