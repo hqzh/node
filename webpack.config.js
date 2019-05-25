f5b91948151db3e8a42dea514b6858239625b1b6
zhuhq <616928019@qq.com>
2019-05-25 22:29:50 +0800

    牛逼的url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,22 @@ module.exports = {
   // 打包一个模块，不知道怎么办的时候配置一个规则告诉它(js默认有配置)，可通过后缀-loader去npm找
   module:{
     rules:[
+      // {
+      //   test:/\.(jpg|png|gif)$/,
+      //   use:{
+      //     loader:'file-loader',
+      //     options:{  //额外参数,placeholder占位符
+      //       name:'[name]_[hash].[ext]', //打包后的名字是原名+hash+后缀
+      //       outputPath:'images/',  // 打包到指定的文件夹
+      //     }
+      //   }
+      // }
       {
-        test:/\.jpg$/,
+        test:/\.(jpg|png|gif)$/,
         use:{
-          loader:'file-loader',
+          loader:'url-loader',
           options:{  //额外参数,placeholder占位符
-            name:'[name]_[hash].[ext]', //打包后的名字是原名+hash+后缀
-            outputPath:'images/',  // 打包到指定的文件夹
+
           }
         }
       }
