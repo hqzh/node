01c94c43c4aef3fca053a1b2cdb4f6b2aba26465
zhuhq <616928019@qq.com>
2019-05-25 21:29:45 +0800

    npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry:'./index.js',
+  entry:'./src/index.js',
   output:{
     filename:'bundle.js',
-    path:path.resolve(__dirname,'bundle')
+    path:path.resolve(__dirname,'dist')
   }
 }
